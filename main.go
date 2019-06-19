df047bb190dfda0c2887b4b9124667ba22d10d05
Elena Kolevska <elena@kolevska.com>
2019-06-19 16:02:46 +0100

    Add go vendor

--- main.go
+++ main.go
@@ -50,6 +50,7 @@ func main() {
 	e := echo.New()
 
 	e.Pre(middleware.AddTrailingSlash())
+	e.Pre(middleware.Recover())
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
 	}))
